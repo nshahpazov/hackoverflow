43ab5e5970980e8b676867c14425e8f8aecec107
Nikola <nshahpazov@gmail.com>
2013-10-26 01:31:57 +0300

    Init configuration for the project

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,30 +1,29 @@
 
 require.config({
-	baseUrl: 'scripts',
-	paths: {
+    baseUrl: 'scripts',
+    paths: {
         jquery: 'libraries/jquery',
         validate: 'libraries/jquery.validate',
         form: 'libraries/jquery.form',
-    	underscore: 'libraries/underscore',
-    	backbone: 'libraries/backbone',
-    	marionette: 'libraries/backbone.marionette',
-    	jade: 'libraries/jade',
+        underscore: 'libraries/underscore',
+        backbone: 'libraries/backbone',
+        marionette: 'libraries/backbone.marionette',
+        jade: 'libraries/jade',
     	text: 'libraries/text',
     	templates: 'templates'
-  	},
-	shim: {
+    },
+    shim: {
 		'backbone': {
 			deps: ['underscore', 'jquery'],
 			exports: 'Backbone'
 		},
-		marionette : {
-      		deps : ['jquery', 'underscore', 'backbone'],
-      		exports : 'Marionette'
-    },
-		
-		'underscore': { exports: '_' },
-    	'jquery': { exports: '$' },
-    	'validate': ['jquery'],
-    	'form': ['jquery']
-	}
+		'marionette' : {
+            deps : ['jquery', 'underscore', 'backbone'],
+            exports : 'Marionette'
+        },
+        'underscore': { exports: '_' },
+        'jquery': { exports: '$' },
+        'validate': ['jquery'],
+        'form': ['jquery']
+    }
 });
