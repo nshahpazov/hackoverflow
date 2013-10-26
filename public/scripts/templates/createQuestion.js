695ba4cb705f35fd025a1c2bb1299ddb829b67eb
Diana Kosashka <diana.kosashka@gmail.com>
2013-10-26 18:47:46 +0300

    added create question template

---
+++ public/scripts/templates/createQuestion.js
@@ -0,0 +1,80 @@
+define(['jade'], function(jade) { if(jade && jade['runtime'] !== undefined) { jade = jade.runtime; }
+
+return function anonymous(locals) {
+jade.debug = [{ lineno: 1, filename: "templates/createQuestion.jade" }];
+try {
+var buf = [];
+jade.debug.unshift({ lineno: 1, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 1, filename: jade.debug[0].filename });
+buf.push("<form method=\"post\" action=\"#\" name=\"input\">");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 2, filename: jade.debug[0].filename });
+buf.push("<div class=\"input-group\">");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 3, filename: jade.debug[0].filename });
+buf.push("<span class=\"input-group-addon\">");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 3, filename: jade.debug[0].filename });
+buf.push("Заглавие: ");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</span>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 4, filename: jade.debug[0].filename });
+buf.push("<input type=\"text\" placeholder=\"Заглавие\" name=\"title\" id=\"title\" class=\"form-control\"/>");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</div>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 5, filename: jade.debug[0].filename });
+buf.push("<div class=\"input-group\">");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 6, filename: jade.debug[0].filename });
+buf.push("<span class=\"input-group-addon\">");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 6, filename: jade.debug[0].filename });
+buf.push("Bъпрос:");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</span>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 7, filename: jade.debug[0].filename });
+buf.push("<textarea placeholder=\"опишете въпроса си тук\" name=\"text\" rows=\"3\" id=\"text\" class=\"form-control\">");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.shift();
+buf.push("</textarea>");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</div>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 8, filename: jade.debug[0].filename });
+buf.push("<div class=\"input-group\">");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 9, filename: jade.debug[0].filename });
+buf.push("<span class=\"input-group-addon\">");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 9, filename: jade.debug[0].filename });
+buf.push("Тагове:");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</span>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 10, filename: jade.debug[0].filename });
+buf.push("<input type=\"text\" placeholders=\"добавете тагове, разделени със запетай\" name=\"tags\" id=\"tags\" class=\"form-control\"/>");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</div>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 11, filename: jade.debug[0].filename });
+buf.push("<input type=\"submit\" value=\"Submit\" class=\"btn btn-primary btn-lg\"/>");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</form>");
+jade.debug.shift();
+jade.debug.shift();;return buf.join("");
+} catch (err) {
+  jade.rethrow(err, jade.debug[0].filename, jade.debug[0].lineno);
+}
+}
+
+});
